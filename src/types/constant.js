8713eb902b9d8921c2f5a2f1809e851e0db93b5c
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-10-09 01:18:27 +0700

    udpate endpoints

--- src/types/constant.js
+++ src/types/constant.js
@@ -1,5 +1,4 @@
-// export const URL = "https://personally-sunny-basilisk.ngrok-free.app/api/v1/";
-export const URL = "https://29b3-115-79-196-67.ngrok-free.app/api/v1/";
+export const URL =  "https://8cc2ee0773b8.ngrok.app/api/v1/";
 // or
 // export const URL = "http://localhost:8888/api/v1/";
 /// 123123123
