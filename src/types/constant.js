3ef7a876ed5625915bb346e85266c5cb0dac5043
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-11-01 09:17:35 +0700

    update endpoint

--- src/types/constant.js
+++ src/types/constant.js
@@ -1,3 +1,3 @@
-// export const URL =  "https://ghoul-helpful-salmon.ngrok-free.app/api/v1/";
-export const URL = "https://103.56.158.105:8888/api/v1/";
+export const URL =  "https://ghoul-helpful-salmon.ngrok-free.app/api/v1/";
+// export const URL = "https://103.56.158.105:8888/api/v1/";
 // export const URL = "http://localhost:8888/api/v1/";
