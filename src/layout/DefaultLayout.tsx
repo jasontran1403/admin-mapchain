8caa2ed3790e4f1d14f3e9a2bfa21a6caee36643
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2025-01-25 22:57:08 +0700

    update layout

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -12,26 +12,31 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <>
       {!isSignInPath ? (
-        <div className="dark:bg-boxdark-2 dark:text-bodydark h-screen">
-          {/* Page Wrapper */}
-          <div className="flex h-full overflow-hidden">
-            {/* Sidebar */}
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+        {/* <!-- ===== Page Wrapper Start ===== --> */}
+        <div className="flex h-screen overflow-hidden">
+          {/* <!-- ===== Sidebar Start ===== --> */}
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Sidebar End ===== --> */}
 
-            {/* Content Area */}
-            <div className="relative flex flex-1 flex-col">
-              {/* Header */}
-              <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Content Area Start ===== --> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* <!-- ===== Header Start ===== --> */}
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* <!-- ===== Header End ===== --> */}
 
-              {/* Main Content */}
-              <main className="flex-1 flex items-center justify-center">
-                <div className="w-full h-full max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                </div>
-              </main>
-            </div>
+            {/* <!-- ===== Main Content Start ===== --> */}
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
+              </div>
+            </main>
+            {/* <!-- ===== Main Content End ===== --> */}
           </div>
+          {/* <!-- ===== Content Area End ===== --> */}
         </div>
+        {/* <!-- ===== Page Wrapper End ===== --> */}
+      </div>
 
       ) : (
         <main>
