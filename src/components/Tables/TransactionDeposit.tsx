3adb45b161b2ee143fe0f525513f5f49bc0f54aa
jasontran1403 <hainguyentran14@gmail.com>
2025-03-02 12:45:34 +0700

    update deposit hash display for bsc

--- src/components/Tables/TransactionDeposit.tsx
+++ src/components/Tables/TransactionDeposit.tsx
@@ -129,12 +129,12 @@ const TransactionDeposit: React.FC<TransactionDepositProps> = ({ data }) => {
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <p className="text-black dark:text-white">
-                    {packageItem.network}
+                    {packageItem.currency === "USDT" ? "Binance Smart Chain" : "TON Network"}
                   </p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <p className="text-black dark:text-white">
-                    {packageItem.network === "Binance Smart Chain" && formatWalletAddress(packageItem.hash)}
+                    {packageItem.currency === "USDT" && formatWalletAddress(packageItem.hash)}
                   </p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
